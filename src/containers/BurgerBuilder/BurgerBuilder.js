65aa7dae2c3994fa3c007183cce1ed66aab50f72
atleastzero <megan.obryan52@gmail.com>
2020-01-03 16:18:25 -0800

    [Not working] working on user adding and subtracting ingredients.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,13 @@ import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3, 
+    bacon: 0.7
+}
+
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
@@ -11,13 +18,41 @@ class BurgerBuilder extends Component {
             bacon: 1,
             cheese: 2,
             meat: 2
-        }
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = type => {
+        const count = this.state.ingredients[type] + 1
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = count;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const price = this.state.totalPrice + priceAddition;
+
+        this.setState({totalPrice: price, ingredients: updatedIngredients})
+    }
+
+    removeIngredientHandler = type => {
+        const count = this.state.ingredients[type] - 1
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = count;
+        const priceSubtraction = INGREDIENT_PRICES[type];
+        const price = this.state.totalPrice - priceSubtraction;
+
+        this.setState({totalPrice: price, ingredients: updatedIngredients})
     }
 
     render() {
         return <Aux>
             <Burger ingredients={this.state.ingredients}/>
-            <BuildControls />
+            <BuildControls 
+                ingredientAdded={this.addIngredientHandler}
+                ingredientRemoved={this.removeIngredientHandler}
+                />
         </Aux>;
     }
 }
